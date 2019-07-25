84545fa4dc25025d74ed411c6098196a2b383355
cllouzeiro <cl.louzeiro@gmail.com>
2019-07-25 18:16:19 -0300

    Atualização na pagina de emolumentos

--- resources/views/site/emolumentos.blade.php
+++ resources/views/site/emolumentos.blade.php
@@ -2,106 +2,122 @@
 
 @section('content')
 
-<section class="pb-5 mt-8">
-	<div class="row col-12 justify-content-center">
-		<div class="col-5">
-			<img  src="{{asset('img/vetores/vetor2.png')}}">
-		</div>
-		<div class="col-5 my-5">
-			<div class="mb-4">
-				<h1 class="text-center">Tabela de Emolumentos</h1>
+
+<section class="emolumentos">
+	<div class="container-fluid">
+		<div class="row mt-5">
+
+			<div class="col-md-6 col-sm-6 col-xs-12 img1">
+				<div class="mt-8 pl-4">
+					<img class="img-fluid" src="{{asset('img/vetores/vetor2.png')}}">
+				</div>
 			</div>
-			<div class="col-12 text-justify">
-				<h5>Aqui você pode encontrar e pesquisar os valores da Tabela de Custas e Emolumentos atualizada, fornecida pelo Tribunal de Justiça.</h5>
+
+			<div class="col-md-6 col-sm-12 col-xs-12 emolumentos__descricao">
+				<div class="mt-10">
+					<h2>Emolumentos</h2>
+				</div>
+				<div class="mt-2">
+					<h5 class="text-muted">Aqui você pode encontrar e pesquisar os valores da Tabela de Custas e Emolumentos atualizada, fornecida pelo Tribunal de Justiça.</h5>
+				</div>
+				<div class="pr-5 pt-4">
+					<form>
+						<div class="form-group">
+							<input class="form-control" type="text" name="pesquisa" placeholder="Digite sua Busca">
+						</div>
+						<div class="form-group">
+							<select class="form-control custom-select">
+								<option>Busca por atribuição</option>
+								<option>Registro de Imóveis</option>
+								<option>Registro de Título e Documentos</option>
+								<option>Tabelionato de Notas</option>
+								<option>Registro Civil das Pessoas Naturais</option>
+								<option>Registro Civil das Pessoas Juridicas</option>
+								<option>Tabelionato de Protesto</option>
+								<option>Tabelionato de Registro de Contratos Marítimos</option>
+								<option>Registro de Distribuição</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-success btn-block">Pesquisar</button>
+						</div>
+					</form>
+				</div>
 			</div>
-			<form class="mb-3">
-				<div class="form">
-					<div class="form-group">
-						<input class="form-control" type="text" name="pesquisa" placeholder="Digite sua Busca">
-					</div>
-					<div class="form-group">
-						<select class="form-control custom-select">
-							<option>Busca por atribuição</option>
-							<option>Registro de Imóveis</option>
-							<option>Registro de Título e Documentos</option>
-							<option>Tabelionato de Notas</option>
-							<option>Registro Civil das Pessoas Naturais</option>
-							<option>Registro Civil das Pessoas Juridicas</option>
-							<option>Tabelionato de Protesto</option>
-							<option>Tabelionato de Registro de Contratos Marítimos</option>
-							<option>Registro de Distribuição</option>
-						</select>
-					</div>
-					<div class="form-group text-center">
-						<button type="submit" class="form-control btn btn-success col-5">Pesquisar</button>
-					</div>
-				</div>	
-			</form>
 		</div>
 	</div>
-	<div class="row col-12 justify-content-center">
-		<div class=" table-responsive col-11">
-		<table class="table table-hover text-center mt-5 bg-light">
-			<thead>
-				<tr>
-					<th>Item</th>
-					<th>Descrição</th>
-					<th>valor</th>
-					<th>Taxa Ferc</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>13.1</td>
-					<td>Escritura Pública com fornecimento do primeiro traslado (com base no valor do ato):</td>
-					<td>0,00</td>
-					<td>0,00</td>
-				</tr>
-				<tr>
-					<td>13.1.1</td>
-					<td>ATÉ R$ 5.100,00</td>
-					<td>103,10</td>
-					<td>3,10</td>
-				</tr>
-				<tr>
-					<td>13.1.10</td>
-					<td>De R$ 37.937,16 a R$ 47.421,43</td>
-					<td>692,50</td>
-					<td>20,80</td>
-				</tr>
-				<tr>
-					<td>13.1.13</td>
-					<td>De R$ 74.095,99 a R$ 92.619,98</td>
-					<td>1.353,10</td>
-					<td>40,60</td>
-				</tr>
-				<tr>
-					<td>13.1.14</td>
-					<td>De R$ 92.619,99 a R$ 115.774,98</td>
-					<td>1.692,90</td>
-					<td>50,80</td>
-				</tr>
-				<tr>
-					<td>13.1.15</td>
-					<td>De R$ 115.774,99 a R$ 144.718,72</td>
-					<td>2.115,40</td>
-					<td>63,50</td>
-				</tr>
-			</tbody>
-		</table>
-		<div class="col-12 mt-3">
-			<nav>
-				<ul class="pagination justify-content-center">
-					<li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-					<li class="page-item"><a class="page-link" href="#">1</a></li>
-					<li class="page-item"><a class="page-link" href="#">2</a></li>
-					<li class="page-item"><a class="page-link" href="#">3</a></li>
-					<li class="page-item"><a class="page-link" href="#">Próximo</a></li>
-				</ul>
-			</nav>
+
+	<div class="row">
+
+		<div class="col-md-12 text-center mt-6">
+			<h3>Tabela de Emolumentos</h3>
+		</div>
+
+		<div class="col-md-12">
+
+			<div class="table-responsive pl-5 pr-5">
+				<table class="table table-hover text-center mt-5 bg-light">
+					<thead>
+						<tr>
+							<th>Item</th>
+							<th>Descrição</th>
+							<th>valor</th>
+							<th>Taxa Ferc</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>13.1</td>
+							<td>Escritura Pública com fornecimento do primeiro traslado (com base no valor do ato):</td>
+							<td>0,00</td>
+							<td>0,00</td>
+						</tr>
+						<tr>
+							<td>13.1.1</td>
+							<td>ATÉ R$ 5.100,00</td>
+							<td>103,10</td>
+							<td>3,10</td>
+						</tr>
+						<tr>
+							<td>13.1.10</td>
+							<td>De R$ 37.937,16 a R$ 47.421,43</td>
+							<td>692,50</td>
+							<td>20,80</td>
+						</tr>
+						<tr>
+							<td>13.1.13</td>
+							<td>De R$ 74.095,99 a R$ 92.619,98</td>
+							<td>1.353,10</td>
+							<td>40,60</td>
+						</tr>
+						<tr>
+							<td>13.1.14</td>
+							<td>De R$ 92.619,99 a R$ 115.774,98</td>
+							<td>1.692,90</td>
+							<td>50,80</td>
+						</tr>
+						<tr>
+							<td>13.1.15</td>
+							<td>De R$ 115.774,99 a R$ 144.718,72</td>
+							<td>2.115,40</td>
+							<td>63,50</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="col-12 mt-3">
+					<nav>
+						<ul class="pagination justify-content-center">
+							<li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+							<li class="page-item"><a class="page-link" href="#">1</a></li>
+							<li class="page-item"><a class="page-link" href="#">2</a></li>
+							<li class="page-item"><a class="page-link" href="#">3</a></li>
+							<li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</div>
 	</div>
-	</div>
-</div>
 </section>
+
 @stop
--- resources/views/site/institucional.blade.php
+++ resources/views/site/institucional.blade.php
@@ -15,17 +15,17 @@
 					<img class="img-institucional" data-aos-duration="2000" data-aos="fade-right" src="{{asset('img/vetores/logo.png')}}">
 				</div>
 				<div class="col-md-7 col-xs-12 text-justify py-5 texto-institucional" data-aos-duration="1000" data-aos="zoom-in">
-					<p>
-						CARTÓRIOS MARANHÃO é a marca da CENTRAL ÚNICA DE SERVIÇOS ELETRÔNICOS COMPARTILHADOS, criada pelo Provimento nº 13/2016 da Corregedoria Geral da Justiça do Maranhão, e mantido pela Associação dos Titulares de Cartórios do Maranhão.<br><br>
+					<p class="text-muted">
+						CARTÓRIOS RORAIMA é a marca da CENTRAL ÚNICA DE SERVIÇOS ELETRÔNICOS COMPARTILHADOS, criada pelo Provimento nº 13/2016 da Corregedoria Geral da Justiça de Roraima, e mantido pela Associação dos Titulares de Cartórios de Maranhão.<br><br>
 
-						Tem a função de centralizar as informações das Serventias Extrajudiciais do Maranhão, conforme determina o Conselho Nacional de Justiça nos Provimentos nº 46, 47 e 48.
+						Tem a função de centralizar as informações das Serventias Extrajudiciais do Roraima, conforme determina o Conselho Nacional de Justiça nos Provimentos nº 46, 47 e 48.
 					</p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-7 col-xs-12 text-justify" data-aos-duration="1000" data-aos="zoom-in" style="margin: auto; font-size: 1.15rem;">
-					<p>
-						Constitui ferramenta destinada a facilitar e modernizar o acesso dos usuários às serventias extrajudiciais maranhenses, mediante a possibilidade de busca de informações, solicitação de certidões, envio e apresentação de documentos (escrituras, instrumentos particulares, e outros documentos gerados eletronicamente).
+					<p class="text-muted pr-4">
+						Constitui ferramenta destinada a facilitar e modernizar o acesso dos usuários às serventias extrajudiciais roraimenses, mediante a possibilidade de busca de informações, solicitação de certidões, envio e apresentação de documentos (escrituras, instrumentos particulares, e outros documentos gerados eletronicamente).
 					</p>
 				</div>
 				<div  class="col-md-5  d-none d-sm-block py-5" style="">
@@ -33,13 +33,13 @@
 				</div>
 			</div>
 			<div class="row justify-content-center">
-				<div  class="col-md-5 d-none d-sm-block py-5" data-aos-duration="2000" data-aos="fade-right">
+				<div  class="col-md-5 mt-5 d-none d-sm-block py-5" data-aos-duration="2000" data-aos="fade-right">
 					<img class="img-institucional" src="{{asset('img/vetores/computer2.png')}}">
 				</div>
-				<div class="col-md-7 col-xs-12 text-justify align-middle" data-aos-duration="1000" data-aos="zoom-in" style="font-size: 1.15rem;">
-					<p class="">
-						A Cartórios Maranhão é responsável por operacionalizar o Sistema de Registro Eletrônico de Imóveis no Estado do Maranhão, previsto pela Lei nº 11.977/2009, que impõe a integração das Serventias de Registro de Imóveis ao universo tecnológico do registro eletrônico. Assim, os usuários poderão apresentar títulos e documentos para registro mediante utilização da Cartórios Maranhão, com a comodidade das ferramentas digitais disponíveis, sem ter que se deslocar fisicamente ao cartório.<br>
-						Acreditamos que a Cartórios Maranhão permitirá aos usuários vivenciarem uma experiência totalmente nova, eficiente e segura. Um dos objetivos da unificação das informações registrais e notariais é de revitalizar a imagem dos Cartórios perante a sociedade, buscando acompanhar o que há de mais avançado em tecnologia web, aliando segurança jurídica, segurança digital e comodidade.
+				<div class="col-md-7 mt-5 col-xs-12 text-justify align-middle" data-aos-duration="1000" data-aos="zoom-in" style="font-size: 1.15rem;">
+					<p class="text-muted">
+						A Cartórios Roraima é responsável por operacionalizar o Sistema de Registro Eletrônico de Imóveis no Estado do Roraima, previsto pela Lei nº 11.977/2009, que impõe a integração das Serventias de Registro de Imóveis ao universo tecnológico do registro eletrônico. Assim, os usuários poderão apresentar títulos e documentos para registro mediante utilização da Cartórios Roraima, com a comodidade das ferramentas digitais disponíveis, sem ter que se deslocar fisicamente ao cartório.<br>
+						Acreditamos que a Cartórios Roraima permitirá aos usuários vivenciarem uma experiência totalmente nova, eficiente e segura. Um dos objetivos da unificação das informações registrais e notariais é de revitalizar a imagem dos Cartórios perante a sociedade, buscando acompanhar o que há de mais avançado em tecnologia web, aliando segurança jurídica, segurança digital e comodidade.
 					</p>
 				</div>
 			</div>
@@ -51,21 +51,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="row justify-content-center fotos-institucional">
-		<div>
-			<img src="{{asset('img/icones/user.png')}}">
-			<h4 class="text-center">Exemplo</h4>
-		</div>
-		<div>
-			<img src="{{asset('img/icones/user.png')}}">
-			<h4 class="text-center">Exemplo</h4>
-		</div>
-		<div>
-			<img src="{{asset('img/icones/user.png')}}">
-			<h4 class="text-center">Exemplo</h4>
-		</div>
-	</div>
-</div>
+
 </section>
 
 @stop
